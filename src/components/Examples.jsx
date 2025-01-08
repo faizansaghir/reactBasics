d2a71340735e2f3e1123ec05350e652c344de682
faizansaghir <dbz.rashidsaghir@gmail.com>
2025-01-08 21:06:06 +0530

    Introduced Generic component of Tabs and concept of multiple JSX slots

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,5 +1,6 @@
 import TabButton from './TabButton'
 import Section from './Section'
+import Tabs from './Tabs'
 
 import { EXAMPLES } from '../data'
 import { useState } from 'react'
@@ -29,13 +30,13 @@ export default function Examples(){
 
     return (
         <Section title='Examples' id='examples'>
-          <menu>
-            <TabButton isSelected={selectedTopic==='components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic==='jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic==='props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic==='state'} onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          {tabContent}
+          <Tabs buttons={
+            <>
+                <TabButton isSelected={selectedTopic==='components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+                <TabButton isSelected={selectedTopic==='jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                <TabButton isSelected={selectedTopic==='props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+                <TabButton isSelected={selectedTopic==='state'} onSelect={() => handleSelect('state')}>State</TabButton>
+            </>} >{tabContent}</Tabs>
         </Section>
     )
 }
